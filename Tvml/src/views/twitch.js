fdca8efd5bea201c92c51fb83934c8d94005afab
Patrick Debois <Patrick.Debois@jedi.be>
2015-12-08 19:27:36 +0100

    add some more player events

--- Tvml/src/views/twitch.js
+++ Tvml/src/views/twitch.js
@@ -1,18 +1,40 @@
-//var homeTemplate = require('../templates/twitch.jade')p
+//var homeTemplate = require('../templates/twitch.jade')
 var Twitch = require('../js/Twitch.js')
 module.exports = function() {
 
   var twitch = new Twitch()
-  twitch.login("wickd", function(err,data) {
-  //twitch.login("yogscast", function(err,data) {
+  //twitch.login("wickd", function(err,data) {
+  twitch.login("yogscast", function(err,data) {
     var VIDEO_URL=twitch.videoUrl()
     //var videoOverlay = Presenter.makeDocument(TVOSTemplates.video());
     var myVideo = new MediaItem('video', VIDEO_URL);
     var player = new Player();
+    player.addEventListener('mediaItemDidChange',function(event) {
+    console.log(event)
+    })
+    player.addEventListener('mediaItemWillChange',function(event) {
+    console.log(event)
+    })
+    player.addEventListener('shouldHandleStateChange',function(event) {
+    console.log(event)
+    })
+    player.addEventListener('stateDidChange',function(event) {
+    console.log(event)
+    })
+    player.addEventListener('stateWillChange',function(event) {
+    console.log(event)
+    })
+    player.addEventListener('requestSeekToTime',function(event) {
+    console.log(event)
+    })
+    // timeBoundaryDidCross
+    // timeDidChange
+    // timedMetadata  
+
     player.playlist = new Playlist();
     player.playlist.push(myVideo);
     //player.overlayDocument = videoOverlay
-    player.present();
+    player.play();
   })
 
 }
